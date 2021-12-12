fd2dd1159fae9cc44b45b0fdf2217eff590e9015
Suraj Das <blogwapz@gmail.com>
2021-12-12 22:37:03 +0530

    add offline cache and fetch logic

--- sw.js
+++ sw.js
@@ -8,4 +8,9 @@ self.addEventListener("install", e => {
 
 self.addEventListener("fetch", e => {
     console.log(`Fetch requesting ${e.request.url}`);
+    e.respondWith(
+        caches.match(e.request).then(response =>{
+            return response || fetch (e.request);
+        })
+    );
 });
